7c1289beb6da4b21c3b7f200d5fa4b1c8eeca616
miguelst1 <miguel_s_t1@hotmail.com>
2018-04-24 09:27:11 +0200

    commit 2

---
+++ src/main/java/es/fpdual/hibernate/web/PersonasManagedBean.java
@@ -0,0 +1,31 @@
+package es.fpdual.hibernate.web;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import hibernate.hibernate.modelo.Persona;
+import hibernate.hibernate.repositorio.RepositorioPersona;
+
+@ManagedBean
+@RequestScoped
+public class PersonasManagedBean implements Serializable {
+
+	private List<Persona> personas;
+
+	public PersonasManagedBean() {
+
+	}
+
+	@PostConstruct
+	public void inicializar() {
+		this.personas = RepositorioPersona.consultar("", "", "", null, "");
+	}
+
+	public List<Persona> getPersonas() {
+		return personas;
+	}
+}
